47ee53d92dace4f44876af2ea70fc8172c6a1aff
Rafal Sikora <24563931+s1korrrr@users.noreply.github.com>
2026-07-15 23:11:39 +0200

    feat: add signed Sparkle update client

--- Sources/PatchwrightApp/App/PatchwrightApp.swift
+++ Sources/PatchwrightApp/App/PatchwrightApp.swift
@@ -5,17 +5,19 @@ import SwiftUI
 struct PatchwrightApp: App {
     @StateObject private var engineProcess: EngineProcessController
     @StateObject private var store: WorkspaceStore
+    @StateObject private var updateController: UpdateController
 
     init() {
         let engineProcess = EngineProcessController()
         _engineProcess = StateObject(wrappedValue: engineProcess)
         _store = StateObject(wrappedValue: WorkspaceStore(engine: UnixEngineClient(socketPath: engineProcess.socketPath)))
+        _updateController = StateObject(wrappedValue: UpdateController())
     }
 
     var body: some Scene {
         WindowGroup("Patchwright") { ContentView(store: store) }
             .defaultSize(width: 1180, height: 760)
-            .commands { PatchwrightCommands(store: store) }
+            .commands { PatchwrightCommands(store: store, updateController: updateController) }
         Settings { SettingsView() }
     }
 }
