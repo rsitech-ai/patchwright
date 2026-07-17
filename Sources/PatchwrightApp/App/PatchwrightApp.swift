112e096210962672a2f90ab35cfd085761bea868
Rafal <mrsikorarafal@gmail.com>
2026-07-17 13:43:26 +0200

    fix(app): stop the owned engine on quit
    Signed-off-by: Rafal <mrsikorarafal@gmail.com>

--- Sources/PatchwrightApp/App/PatchwrightApp.swift
+++ Sources/PatchwrightApp/App/PatchwrightApp.swift
@@ -1,8 +1,19 @@
+import AppKit
 import PatchwrightCore
 import SwiftUI
 
+@MainActor
+final class PatchwrightApplicationDelegate: NSObject, NSApplicationDelegate {
+    static weak var engineProcessController: EngineProcessController?
+
+    func applicationWillTerminate(_: Notification) {
+        Self.engineProcessController?.shutdown()
+    }
+}
+
 @main
 struct PatchwrightApp: App {
+    @NSApplicationDelegateAdaptor(PatchwrightApplicationDelegate.self) private var appDelegate
     @StateObject private var engineProcess: EngineProcessController
     @StateObject private var store: WorkspaceStore
     @StateObject private var updateController: UpdateController
@@ -12,6 +23,7 @@ struct PatchwrightApp: App {
         _engineProcess = StateObject(wrappedValue: engineProcess)
         _store = StateObject(wrappedValue: WorkspaceStore(engine: UnixEngineClient(socketPath: engineProcess.socketPath)))
         _updateController = StateObject(wrappedValue: UpdateController())
+        PatchwrightApplicationDelegate.engineProcessController = engineProcess
     }
 
     var body: some Scene {
